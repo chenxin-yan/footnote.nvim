d6cfde4764d98cc9428ed322c5dddf76d353a794
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-08-07 23:20:53 -0400

    implement M.next_footnote()

--- lua/footnote.lua
+++ lua/footnote.lua
@@ -58,8 +58,45 @@ function M.organize_footnotes()
   -- TODO: implement organize footnotes
 end
 
+--- Get the location of next footnote ref
+---@param bufnr number the buffer numer (0 for current buffer)
+---@param row number the row of current cursor
+---@param col number the col of current cursor
+---@return table | nil refLocation  location of the next footnote in a table {row, col}. If not found, return 'nil'
+local function find_next(bufnr, row, col)
+  local buffer = vim.api.nvim_buf_get_lines(bufnr, row - 1, -1, false)
+  buffer[1] = string.sub(buffer[1], col + 1, -1)
+  for i, line in ipairs(buffer) do
+    if string.find(line, '^%[%^%d+%]:') then
+      return nil
+    end
+    while true do
+      local refCol = string.find(line, '%[%^%d+]')
+      if refCol == nil then
+        break
+      end
+      if i == 1 then
+        refCol = refCol + col
+      end
+      return { row - 1 + i, refCol + 1 }
+    end
+  end
+  return nil
+end
+
 function M.next_footnote()
-  -- TODO: implement footnote navigation
+  local cursor_pos = vim.api.nvim_win_get_cursor(0)
+  local row = cursor_pos[1]
+  local col = cursor_pos[2]
+
+  -- get the index of the start of next footnote ref
+  local refLocation = find_next(0, row, col)
+  if refLocation == nil then
+    return
+  end
+
+  -- move cursor to the next footnote ref
+  vim.api.nvim_win_set_cursor(0, { refLocation[1], refLocation[2] })
 end
 
 function M.prev_footnote()

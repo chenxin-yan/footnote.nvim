50d58e3525cf1560fed3947d67c1ac41c6fe3594
Chenxin Yan <yanchenxin2004@gmail.com>
2025-06-13 17:36:46 -0400

    handle negative footnote numbers

--- lua/footnote/organizer.lua
+++ lua/footnote/organizer.lua
@@ -1,19 +1,77 @@
 local M = {}
 
---- rename all footnote references with given label to another label
+--- Mark a footnote reference as orphan
 ---@param bufnr number buffer number
 ---@param ref_locations table locations of all the footnote references
 ---@param orphan_locations table locations of all the orphan references
+---@param from number the label to mark as orphan
+local function mark_as_orphan(bufnr, ref_locations, orphan_locations, from)
+  -- If number is already negative, keep it negative; otherwise make it negative
+  local orphan_number = from < 0 and from or -from
+
+  local buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local matching_locations = {}
+
+  -- First pass: collect all matching locations
+  for index = 1, #ref_locations, 1 do
+    local location = ref_locations[index]
+    if location == nil then
+      goto continue
+    end
+    local label = string.sub(buffer[location[1]], location[2], location[3])
+    local number = tonumber(string.sub(label, 3, -2))
+
+    if number == from then
+      matching_locations[#matching_locations + 1] = { index, location }
+    end
+
+    ::continue::
+  end
+
+  -- Second pass: process all matching locations (reverse order to avoid position corruption)
+  for i = #matching_locations, 1, -1 do
+    local index, location = matching_locations[i][1], matching_locations[i][2]
+    local row = location[1]
+    local startCol = location[2]
+    local endCol = location[3]
+
+    -- Rename to negative number
+    vim.api.nvim_buf_set_text(bufnr, row - 1, startCol + 1, row - 1, endCol - 1, { tostring(orphan_number) })
+
+    -- HACK: endcol + diff because if "from" is orphan
+    -- the number will have a "-" prepend to the number after rename
+    local diff = #tostring(orphan_number) - #tostring(from)
+    orphan_locations[#orphan_locations + 1] = { row, startCol, endCol + diff }
+
+    -- Update ref_locations for this change
+    local shift = #tostring(orphan_number) - #tostring(from)
+    if shift ~= 0 then
+      ref_locations[index][3] = ref_locations[index][3] + shift
+      -- Update positions of references that come after this one on the same row
+      for j = 1, #ref_locations do
+        local next_location = ref_locations[j]
+        if next_location ~= nil and next_location[1] == row and next_location[2] > startCol then
+          ref_locations[j][2] = ref_locations[j][2] + shift
+          ref_locations[j][3] = ref_locations[j][3] + shift
+        end
+      end
+    end
+
+    -- Refresh buffer for next iteration
+    buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  end
+end
+
+--- rename all footnote references with given label to another label
+---@param bufnr number buffer number
+---@param ref_locations table locations of all the footnote references
 ---@param from number the label to change
 ---@param to number the label to change to
-local function ref_rename(bufnr, ref_locations, orphan_locations, from, to)
+local function ref_rename(bufnr, ref_locations, from, to)
   if from == to then
     return
   end
   local buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-
-  -- HACK: currently, orphan detected were marked as negatives
-  local isOrphan = to < 0
 
   for index = 1, #ref_locations, 1 do
     local location = ref_locations[index]
@@ -35,12 +93,6 @@ local function ref_rename(bufnr, ref_locations, orphan_locations, from, to)
       end
       shift = #tostring(to) - #tostring(from)
       vim.api.nvim_buf_set_text(bufnr, row - 1, startCol + 1, row - 1, endCol - 1, { tostring(to) })
-
-      if isOrphan then
-        -- HACK: endcol + 1 because if "from"" is orphan
-        -- the number will have a "-" prepend to the number after rename
-        orphan_locations[#orphan_locations + 1] = { row, startCol, endCol + 1 }
-      end
     elseif number == to then
       if Opts.debug_print then
         print('ref_rename: ' .. to .. ' -> ' .. from)
@@ -78,14 +130,14 @@ local function content_rename(bufnr, content_locations, from, to)
   end
   local buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
   for _, row in ipairs(content_locations) do
-    local num = string.match(buffer[row], '%d+')
+    local num = string.match(buffer[row], '%-?%d+')
     if tonumber(num) == from then
-      local i, j = string.find(buffer[row], '%d+')
+      local i, j = string.find(buffer[row], '%-?%d+')
       if i and j then
         vim.api.nvim_buf_set_text(bufnr, row - 1, i - 1, row - 1, j, { tostring(to) })
       end
     elseif tonumber(num) == to then
-      local i, j = string.find(buffer[row], '%d+')
+      local i, j = string.find(buffer[row], '%-?%d+')
       if i and j then
         vim.api.nvim_buf_set_text(bufnr, row - 1, i - 1, row - 1, j, { tostring(from) })
       end
@@ -95,15 +147,13 @@ end
 
 -- check to see if a given footnote is an orphan
 ---@param bufnr number buffer number
----@param ref_locations table locations of all the footnote references
 ---@param content_locations table locations of all the footnote content
----@param is_deleted table flags of whether a given footnote reference is deleted
 ---@param from number the refernce label to be checked
-local function is_orphan(bufnr, ref_locations, content_locations, from)
+local function is_orphan(bufnr, content_locations, from)
   local buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
   local isOrphan = true
   for _, row in ipairs(content_locations) do
-    local num = tonumber(string.match(buffer[row], '%d+'))
+    local num = tonumber(string.match(buffer[row], '%-?%d+'))
     if num == from then
       isOrphan = false
       break
@@ -122,7 +172,8 @@ end
 ---@param orphan_locations table locations of all the orphans tha needed to be cleaned up
 local function cleanup_orphan(bufnr, orphan_locations)
   local buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  for index = 1, #orphan_locations, 1 do
+  -- Process orphans in reverse order to avoid position corruption
+  for index = #orphan_locations, 1, -1 do
     local location = orphan_locations[index]
     if location == nil then
       goto continue
@@ -136,20 +187,6 @@ local function cleanup_orphan(bufnr, orphan_locations)
     buffer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
     vim.notify('label "' .. label .. '" at row ' .. row .. ' is deleted', vim.log.levels.INFO)
-
-    local shift = endCol - startCol + 1
-
-    for j = index, #orphan_locations, 1 do
-      local next_location = orphan_locations[j + 1]
-      if next_location == nil or orphan_locations[1] ~= row then
-        break
-      end
-      orphan_locations[j + 1][2] = orphan_locations[j + 1][2] - shift
-      orphan_locations[j + 1][3] = orphan_locations[j + 1][3] - shift
-      if Opts.debug_print then
-        print('shifted(' .. shift .. '): ' .. orphan_locations[j + 1][1] .. ', ' .. orphan_locations[j + 1][2] .. ':' .. orphan_locations[j + 1][3])
-      end
-    end
     ::continue::
   end
 end
@@ -166,7 +203,7 @@ function M.organize_footnotes(skip_colon_check)
   local missing_colon_locations = {}
 
   for i, line in ipairs(buffer) do
-    if string.find(line, '^%[%^%d+%]:') then
+    if string.find(line, '^%[%^%-?%d+%]:') then
       content_locations[#content_locations + 1] = i
       goto continue
     end
@@ -174,7 +211,7 @@ function M.organize_footnotes(skip_colon_check)
     -- NOTE: see issue #2
     -- Check if line starts with [^d] without colon
     if not skip_colon_check then
-      local footnote_start, footnote_end = string.find(line, '^%[%^%d+%]')
+      local footnote_start, footnote_end = string.find(line, '^%[%^%-?%d+%]')
       if footnote_start and footnote_end then
         missing_colon_locations[#missing_colon_locations + 1] = { i, footnote_end }
         goto continue
@@ -185,7 +222,7 @@ function M.organize_footnotes(skip_colon_check)
     local refEnd = nil
     while true do
       ---@diagnostic disable-next-line: cast-local-type
-      refStart, refEnd = string.find(line, '%[%^%d+%]', refStart + 1)
+      refStart, refEnd = string.find(line, '%[%^%-?%d+%]', refStart + 1)
       if refStart == nil or refEnd == nil then
         break
       end
@@ -235,26 +272,32 @@ function M.organize_footnotes(skip_colon_check)
     return
   end
 
-  -- iterate footnote and sort labels
-  local counter = 1
+  -- First pass: collect all unique footnote numbers from original buffer
+  local unique_numbers = {}
+  local seen_numbers = {}
   for index = 1, #ref_locations, 1 do
     local location = ref_locations[index]
-    buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
     local label = string.sub(buffer[location[1]], location[2], location[3])
     local number = tonumber(string.sub(label, 3, -2))
 
-    -- Process foonotes
-    if number and number >= counter then
-      if not is_orphan(0, ref_locations, content_locations, number) then
-        if Opts.debug_print then
-          print(number .. ' -> ' .. counter)
-        end
-        ref_rename(0, ref_locations, orphan_locations, number, counter)
-        content_rename(0, content_locations, number, counter)
-      else
-        ref_rename(0, ref_locations, orphan_locations, number, -number)
+    if number and not seen_numbers[number] then
+      seen_numbers[number] = true
+      unique_numbers[#unique_numbers + 1] = number
+    end
+  end
+
+  -- Second pass: process each unique number once
+  local counter = 1
+  for _, number in ipairs(unique_numbers) do
+    if not is_orphan(0, content_locations, number) then
+      if Opts.debug_print then
+        print(number .. ' -> ' .. counter)
       end
+      ref_rename(0, ref_locations, number, counter)
+      content_rename(0, content_locations, number, counter)
       counter = counter + 1
+    else
+      mark_as_orphan(0, ref_locations, orphan_locations, number)
     end
   end
 
@@ -269,7 +312,7 @@ function M.organize_footnotes(skip_colon_check)
     local target = content_locations[i]
     for j = i, #content_locations, 1 do
       local current = content_locations[j]
-      local num = string.match(buffer[current], '%d+')
+      local num = string.match(buffer[current], '%-?%d+')
       if tonumber(num) == i and j ~= i then
         local temp = buffer[target]
         vim.api.nvim_buf_set_text(0, target - 1, 0, target - 1, -1, { buffer[current] })
